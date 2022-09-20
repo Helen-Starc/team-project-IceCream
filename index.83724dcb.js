5240567302b676e227c19d30ff2fbce7c0a58953
Helen-Starc <109182577+Helen-Starc@users.noreply.github.com>
2022-09-20 18:29:20 +0000

    Deploying to gh-pages from @ Helen-Starc/team-project-IceCream@d54020e7cfab2f42443a2f6aa5236286fbe04850 🚀

--- index.83724dcb.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),backdrop:document.querySelector("[data-backdrop]")};function o(){document.body.classList.toggle("modal-open"),e.backdrop.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.83724dcb.js.map
