fe2850ed062582301e86c3a81362aae3ae392854
Lary <110429721+Laryk96@users.noreply.github.com>
2022-09-20 22:19:53 +0300

    add js for mobail-logo

--- src/js/mobail-menu.js
+++ src/js/mobail-menu.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const logoindent = document.querySelector('[data-logo]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
@@ -8,6 +9,7 @@
 
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
+    logoindent.classList.toggle('indent');
 
     mobileMenuRef.classList.toggle('is-open');
   });
