d54020e7cfab2f42443a2f6aa5236286fbe04850
Helen-Starc <109182577+Helen-Starc@users.noreply.github.com>
2022-09-20 20:28:38 +0200

    update header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
-import './js/mobail-menu';
-import './js/modal window';
+(() => {
+  //перменные (константы), которые связываны с элементами, на которых есть соответствующие атрибуты
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    backdrop: document.querySelector('[data-backdrop]'),
+  };
+
+  //эдвэнтлистэнэр, который при клике активирует функцию "переключения"
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  //функция переключения классов
+  function toggleModal() {
+    //вешает на боди 'modal-open'
+    document.body.classList.toggle('modal-open');
+    //вешает на бєкдроп 'is-hidden'
+    refs.backdrop.classList.toggle('is-hidden');
+  }
+})();
+
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
