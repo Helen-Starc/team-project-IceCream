232b60bd1a86d3e139ba21ec0eec0b38a3900ad7
Lary <110429721+Laryk96@users.noreply.github.com>
2022-09-21 01:30:33 +0300

    Update mobail.js

--- src/partials/js/mobail.js
+++ src/partials/js/mobail.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const logoindent = document.querySelector('[data-logo]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
@@ -8,7 +9,33 @@
 
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
+    logoindent.classList.toggle('indent');
 
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
+
+// Modal
+
+(() => {
+  //перменные (константы), которые связываны с элементами, на которых есть соответствующие атрибуты
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    backdrop: document.querySelector('[data-backdrop]'),
+  };
+
+  //эдвэнтлистэнэр, который при клике активирует функцию "переключения"
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  //функция переключения классов
+  function toggleModal() {
+    //вешает на боди 'modal-open'
+    document.body.classList.toggle('modal-open');
+    //вешает на бєкдроп 'is-hidden'
+    refs.backdrop.classList.toggle('is-hidden');
+  }
+})();
+
+// Mobile menu
