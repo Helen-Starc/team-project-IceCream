828fb94d29d10bef3bc48b091c461a10590caba7
Helen-Starc <109182577+Helen-Starc@users.noreply.github.com>
2022-09-22 17:08:38 +0200

    updated

--- src/partials/js/mob-menu.js
+++ src/partials/js/mob-menu.js
@@ -3,8 +3,60 @@
   const mobileMenuRef = document.querySelector('[data-menu]');
   const logoindent = document.querySelector('[data-logo]');
   const body = document.querySelector('body');
+  const menuBtnNav1 = document.querySelector('[menu-btn-nav-data1]');
+  const menuBtnNav2 = document.querySelector('[menu-btn-nav-data2]');
+  const menuBtnNav3 = document.querySelector('[menu-btn-nav-data3]');
+  const menuBtnNav4 = document.querySelector('[menu-btn-nav-data4]');
 
   menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    logoindent.classList.toggle('indent');
+
+    mobileMenuRef.classList.toggle('is-open');
+    body.classList.toggle('menu-open');
+  });
+
+  menuBtnNav1.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    logoindent.classList.toggle('indent');
+
+    mobileMenuRef.classList.toggle('is-open');
+    body.classList.toggle('menu-open');
+  });
+
+  menuBtnNav2.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    logoindent.classList.toggle('indent');
+
+    mobileMenuRef.classList.toggle('is-open');
+    body.classList.toggle('menu-open');
+  });
+
+  menuBtnNav3.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    logoindent.classList.toggle('indent');
+
+    mobileMenuRef.classList.toggle('is-open');
+    body.classList.toggle('menu-open');
+  });
+
+  menuBtnNav4.addEventListener('click', () => {
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
